0d1f647931d9321695523758d3c3d5e97371f9df
eddierodz7 <eddierodz7@gmail.com>
2018-10-01 10:30:49 -0500

    new repo

---
+++ thepoolmanager/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <p>This is the landing page</p>
+      </div>
+    );
+  }
+}
+
+export default App;
