a6baf4a135d8b0b959c2b6486444103ef0acf902
Juan Guevara <juanguevara210463@gmail.com>
2018-10-02 15:42:48 -0500

    Routs done

--- thepoolmanager/src/index.js
+++ thepoolmanager/src/index.js
@@ -1,7 +1,33 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App.js';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import {Component} from 'react-dom';
+import {Link} from "./component/landingPage.jsx";
+import App from './component/App.js';
+import registerServiceWorker from './component/registerServiceWorker';
+
+//import Form from "./components/form.js";
+
+
+class Index extends React.Component {
+     render() {
+      <div className='Landing Page'>
+        <BrowserRouter>
+          <div>
+            <Route path='/' component={App} />
+            <Route path='/Home' component={Home} />
+            <Route exact path='/About' component={About} />
+            <Route exact path='/Events' component={Events} />
+            <Route exact path='/BookAVisit' component={BookAVisit} />
+            <Route exact path='/registerServiceWorker' component={registerServiceWorker} />
+         </div>
+       </BrowserRouter>
+      </div>
+
+
+
+
+
+
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
