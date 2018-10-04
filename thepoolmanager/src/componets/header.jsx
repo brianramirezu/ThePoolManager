07ee291ea3415506cc2891e207b06f6a2ee6f213
eddierodz7 <eddierodz7@gmail.com>
2018-10-04 12:24:45 -0500

    got bootstrap header working

--- thepoolmanager/src/componets/header.jsx
+++ thepoolmanager/src/componets/header.jsx
@@ -1,18 +1,36 @@
 import React from "react";
 import Events from "./Events.jsx";
 import {NavLink}  from 'react-router-dom';
+import {Navbar, NavItem, NavDropdown, Nav, MenuItem} from 'react-bootstrap';
 
-const Header = () => {
+const HeaderBar = () => {
   return (
-    <div className ="header">
-      <ul>
-      <NavLink to="/">Inicio</NavLink>
-      <NavLink to="/servicios">Servicios</NavLink>
-      <NavLink to="/eventos">Eventos</NavLink>
-      <NavLink to="/visita">Solicita tu visita</NavLink>
-      <NavLink to="/constactos">Contactos</NavLink>
-</ul>
+    <div>
+
+
+  <nav className="navbar navbar-inverse">
+    <div className="container-fluid">
+      <div className="navbar-header">
+        <a className="navbar-brand" href="#">The Pool Manager</a>
       </div>
+      <ul className="nav navbar-nav">
+        <li className="active"><a href="#">Home</a></li>
+        <li className="dropdown"><a className="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span className="caret"></span></a>
+          <ul className="dropdown-menu">
+            <li><a href="#">Page 1-1</a></li>
+            <li><a href="#">Page 1-2</a></li>
+            <li><a href="#">Page 1-3</a></li>
+          </ul>
+        </li>
+        <li><a href="#">Page 2</a></li>
+      </ul>
+      <ul className="nav navbar-nav navbar-right">
+        <li><a href="#"><span className="glyphicon glyphicon-user"></span> Sign Up</a></li>
+        <li><a href="#"><span className="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+    </div>
+  </nav>
+    </div>
     )
   }
-  export default Header;
+  export default HeaderBar;
